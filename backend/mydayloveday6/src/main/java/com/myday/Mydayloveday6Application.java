0afd2a786e1df9439f1dd94a56f44806577159f4
Jangchaeyun <zzangsally@naver.com>
2023-12-27 23:34:05 +0900

    1227 Backend Project

---
+++ backend/mydayloveday6/src/main/java/com/myday/Mydayloveday6Application.java
@@ -0,0 +1,13 @@
+package com.myday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mydayloveday6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mydayloveday6Application.class, args);
+	}
+
+}
