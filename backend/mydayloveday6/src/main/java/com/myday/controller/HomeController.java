de448be217deb1480c6c26a4ff513d86409a1566
Jangchaeyun <zzangsally@naver.com>
2023-12-28 23:11:44 +0900

    1228 HomeController Backend

---
+++ backend/mydayloveday6/src/main/java/com/myday/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.myday.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping
+	public String homeControllerHandler() {
+		return "this is home controller";
+	}
+	
+	@GetMapping("/home")
+	public String homeControllerHandler2() {
+		return "this is home controller 2";
+	}
+	
+	@GetMapping("/mydayloveday6")
+	public String homeControllerHandler3() {
+		return "hello mydayloveday6";
+	}
+	
+//	@PutMapping
+	
+//	@PostMapping
+	
+//	@DeleteMapping
+	
+}
