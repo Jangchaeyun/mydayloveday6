56595a07624bb0e7ac721bde6c1b726939ee67ca
Jangchaeyun <zzangsally@naver.com>
2023-12-30 23:24:57 +0900

    1230 User Controller

--- backend/mydayloveday6/src/main/java/com/myday/controller/UserController.java
+++ backend/mydayloveday6/src/main/java/com/myday/controller/UserController.java
@@ -3,9 +3,11 @@ package com.myday.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,5 +46,26 @@ public class UserController {
 		newUser.setId(user.getId());
 		
 		return newUser;
+	}
+	
+	@PutMapping("/users")
+	public User updateUser(@RequestBody User user) {
+		User user1 = new User(1, "myday", "love", "mydayloveday6@gmail.com", "12345");
+		if(user.getFirstName() != null) {
+			user1.setFirstName(user.getFirstName());
+		}
+		if (user.getLastName() != null) {
+			user1.setLastName(user.getLastName());
+		}
+		if (user.getEmail() != null) {
+			user1.setEmail(user.getEmail());
+		}
+		
+		return user1;
+	}
+	
+	@DeleteMapping("/users/{userId}")
+	public String deleteUser(@PathVariable("userId") Integer userId) {
+		return "user deleted successfully with id " + userId;
 	}
 }
