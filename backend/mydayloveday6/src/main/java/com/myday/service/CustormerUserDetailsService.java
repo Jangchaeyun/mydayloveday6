eb92b14409750e591a0a11d07b09fe87392139a3
Jangchaeyun <zzangsally@naver.com>
2024-01-17 23:30:56 +0900

    0117 CustormerUserDetailsService

---
+++ backend/mydayloveday6/src/main/java/com/myday/service/CustormerUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.myday.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.myday.models.User;
+import com.myday.repository.UserRepository;
+
+@Service
+public class CustormerUserDetailsService implements UserDetailsService {
+	@Autowired
+	private UserRepository userRepository;
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username);
+		
+		if (user == null) {
+			throw new UsernameNotFoundException("user not found with email + " + username);
+		}
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+	}
+
+}
