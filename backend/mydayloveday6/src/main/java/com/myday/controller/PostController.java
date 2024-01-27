8049789c9c46de4a10040c9cb3f91cc132606ea2
Jangchaeyun <zzangsally@naver.com>
2024-01-27 22:52:28 +0900

    0127 CreatePost JWT Update

--- backend/mydayloveday6/src/main/java/com/myday/controller/PostController.java
+++ backend/mydayloveday6/src/main/java/com/myday/controller/PostController.java
@@ -11,20 +11,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.myday.models.Post;
+import com.myday.models.User;
 import com.myday.response.ApiResponse;
 import com.myday.service.PostService;
+import com.myday.service.UserService;
 
 @RestController
 public class PostController {
 	@Autowired
 	PostService postService;
 	
-	@PostMapping("/posts/user/{userId}")
-	public ResponseEntity<Post> createPost(@RequestBody Post post, @PathVariable Integer userId) throws Exception {
-		Post createdPost = postService.createNewPost(post, userId);
+	@Autowired
+	UserService userService;
+	
+	@PostMapping("/api/posts")
+	public ResponseEntity<Post> createPost(@RequestHeader("Authorization") String jwt, @RequestBody Post post) throws Exception {
+		User reqUser = userService.findUserByJwt(jwt);
+		Post createdPost = postService.createNewPost(post, reqUser.getId());
 		return new ResponseEntity<>(createdPost, HttpStatus.ACCEPTED);
 	}
 	
