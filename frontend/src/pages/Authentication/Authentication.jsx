8502aee7ea6fc56f9960050a4e32055d3032ff23
Jangchaeyun <zzangsally@naver.com>
2024-02-04 22:49:50 +0900

    0204 CreateReactApp & Authentication

---
+++ frontend/src/pages/Authentication/Authentication.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+
+const Authentication = () => {
+  return (
+    <div>
+      <Grid container>
+        <Grid className="h-screen overflow-hidden" item xs={7}>
+          <img
+            className="h-full w-full justify-center align-middle"
+            src="https://ifh.cc/g/O0NxKs.jpg"
+            alt=""
+          />
+        </Grid>
+        <Grid item xs={5}></Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Authentication;
