a40bfd161ff1edaa8050d94fee75bf4cff0f993c
Jangchaeyun <zzangsally@naver.com>
2024-02-07 18:53:57 +0900

    0207 SideBar

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,70 @@
 import React from "react";
+import { navigationMenu } from "./SidebarNavigation";
+import { Divider, Avatar, Card } from "@mui/material";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 const Sidebar = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <div className="card h-screen flex flex-col justify-between py-5">
+    <Card className="card h-screen flex flex-col justify-between py-5">
       <div className="space-y-8 pl-5">
         <div className="">
           <span className="logo font-bold text-xl">MydayLoveDay6</span>
         </div>
+        <div className="space-y-8">
+          {navigationMenu.map((item) => (
+            <div className="cursor-pointer flex space-x-3 items-center">
+              {item.icon}
+              <p className="text-xl">{item.title}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      <div>
+        <Divider />
+        <div className="pl-5 flex items-center justify-between pt-5">
+          <div className="flex items-center space-x-3">
+            <Avatar src="https://i.namu.wiki/i/P2JXK0XHZDt7RNrs2oqNilKr6N5scsfALk2dRzcGqg018BwRPYXllLqb0eXf77kVHeRhylLl-yfZUup_Ds5KiAYi3CPS35qpfN5OGJdazQDlJxnU1XquKiEg78Ab3Rp1o8yn-ITIqVYIkLe5EKW_-g.webp" />
+            <div>
+              <p className="font-bold">MydayForever</p>
+              <p className="opacity-70">@mydayforever</p>
+            </div>
+          </div>
+          <Button
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleOpen}
+          >
+            <MoreHorizIcon />
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleClose}>프로필</MenuItem>
+            <MenuItem onClick={handleClose}>나의 계정</MenuItem>
+            <MenuItem onClick={handleClose}>로그아웃</MenuItem>
+          </Menu>
+        </div>
+      </div>
+    </Card>
   );
 };
 
