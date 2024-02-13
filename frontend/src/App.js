0bcba804dc42f0bbc9306b2fe6a62f033bb41f37
Jangchaeyun <zzangsally@naver.com>
2024-02-13 22:35:39 +0900

    0213 ProfileActionType

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,26 @@ import "./App.css";
 import Authentication from "./pages/Authentication/Authentication";
 import Message from "./pages/Message/Message";
 import HomePage from "./pages/HomePage/HomePage";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getProfileAction } from "./Redux/Auth/auth.action";
 
 function App() {
+  const dispatch = useDispatch();
+  const { auth } = useSelector((store) => store);
+  const jwt = localStorage.getItem("jwt");
+
+  useEffect(() => {
+    dispatch(getProfileAction(jwt));
+  }, [jwt]);
+
   return (
     <div className="">
       <Routes>
-        <Route path="/*" element={<HomePage />} />
+        <Route
+          path="/*"
+          element={auth.user ? <HomePage /> : <Authentication />}
+        />
         <Route path="/message" element={<Message />} />
         <Route path="/*" element={<Authentication />} />
       </Routes>
