561d45cd3fd2aa1a118b9feea458f233e69e01e9
Jangchaeyun <zzangsally@naver.com>
2024-03-17 16:16:06 +0900

    0317 MessageAction

--- frontend/src/Redux/Message/message.action.js
+++ frontend/src/Redux/Message/message.action.js
@@ -12,3 +12,27 @@ export const createMessage = (message) => async (dispatch) => {
     dispatch({ tyoe: actionType.CREATE_MESSAGE_FAILURE, payload: error });
   }
 };
+
+export const createChat = (chat) => async (dispatch) => {
+  dispatch({ type: actionType.CREATE_CHAT_FAILURE });
+  try {
+    const { data } = await api.post(`/api/chats`, chat);
+    console.log("create message ", data);
+    dispatch({ type: actionType.CREATE_CHAT_SUCCESS, payload: data });
+  } catch (error) {
+    console.log("CATCH ERROR ", error);
+    dispatch({ tyoe: actionType.CREATE_CHAT_FAILURE, payload: error });
+  }
+};
+
+export const getAllChats = (message) => async (dispatch) => {
+  dispatch({ type: actionType.GET_ALL_CHATS_REQUEST });
+  try {
+    const { data } = await api.post(`/api/chats/user`, message);
+    console.log("get all chats ", data);
+    dispatch({ type: actionType.GET_ALL_CHATS_SUCCESS, payload: data });
+  } catch (error) {
+    console.log("CATCH ERROR ", error);
+    dispatch({ tyoe: actionType.GET_ALL_CHATS_FAILURE, payload: error });
+  }
+};
